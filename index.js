8ae9690dc96b8e5b6698083ebaf7336aef386a40
André Vinícius Matioski <88065540+AndreMatioski02@users.noreply.github.com>
2022-09-24 22:55:59 -0300

    Alterações e adição de rotas

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const config = require("./config");
 const produtoRoutes = require("./routes/produtoRoutes");
 const servicoRoutes = require("./routes/servicoRoutes");
 const quartoRoutes = require("./routes/quartoRoutes");
+const colaboradorRoutes = require("./routes/colaboradorRoutes");
+const animalRoutes = require("./routes/animalRoutes");
+const clienteRoutes = require("./routes/clienteRoutes");
 
 const app = express();
 
@@ -13,8 +16,11 @@ app.use(express.json());
 
 app.use(cors());
 
-app.use("/api/produtos", produtoRoutes.routes);
-app.use("/api/servicos", servicoRoutes.routes);
-app.use("/api/quartos", quartoRoutes.routes);
+app.use("/api/produtos", produtoRoutes)
+app.use("/api/servicos", servicoRoutes)
+app.use("/api/quartos", quartoRoutes)
+app.use('/api/colaboradores', colaboradorRoutes)
+app.use('/api/animais', animalRoutes)
+app.use('/api/clientes', clienteRoutes)
 
 app.listen(config.port, () => console.log(`API está rodando em http://localhost:${config.port}`));
